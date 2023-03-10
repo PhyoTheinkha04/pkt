283f052f760a8ee55b24b72e4bdea8c3d5a6f29f
PhyoTheinkha04 <kokha042@gmail.com>
2023-03-10 17:32:34 +0630

    pkt course and tainers section

--- js/animate.js
+++ js/animate.js
@@ -1,58 +1,20 @@
-// This is setup with bootstrap 3
-
-/**
-* If you have LESS than one navbar, then just do 
-* let menu = document.getElementsByClassName( 'nav' )[0];
-*/ 
-let menu = document.getElementsByClassName( 'nav' );
-// Values are set.
-if ( menu ) {
-  // CLICK
-  let menu_slider_click = document.getElementById( 'nav_slide_click' );
-  if ( menu_slider_click ) {
-    nav_slider( menu[1], function( el, width, tempMarginLeft ) {                          
-      el.onclick = () => {
-        menu_slider_click.style.width =  width + '%';                    
-        menu_slider_click.style.marginLeft = tempMarginLeft + '%';    
-      }
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += increment;
+     obj.textContent = current;
+     if (current == end) {
+      clearInterval(timer);
+     }
+    }, step);
   }
+  counter("count1", 0, 500, 3000);
+  counter("count2", 100, 1550, 2500);
+  counter("count3", 0, 7, 3000);
+ });
  
-} // endif
-
-function nav_slider( menu, callback ) {
-  let menu_width = menu.offsetWidth;
-  // only want the <li> </li> tags
-  menu = menu.getElementsByTagName( 'li' );            
-  if ( menu.length > 0 ) {
-    var marginLeft = [];
-    // Loop through nav children i.e li
-    [].forEach.call( menu, ( el, index ) => {
-      // Dynamic width/margin calculation for hr              
-      var width = getPercentage( el.offsetWidth, menu_width );                              
-      var tempMarginLeft = 0;
-      // We don't want to modify first elements positioning
-      if ( index != 0 )  {
-        tempMarginLeft = getArraySum( marginLeft );
-      }          tempMarginLeft  
-      // Set mouse event  hover/click
-      callback( el, width, tempMarginLeft );      
-      /* We store it in array because the later accumulated value is used for positioning */
-      marginLeft.push( width );
-    } );
-  }
-}
-
-// Might make this dynamic for px, %, pt, em 
-function getPercentage( min, max ) {
-  return min / max * 100;
-}
-
-// Not using reduce, because IE8 doesn't supprt it
-function getArraySum( arr ) {
-  let sum = 0;
-  [].forEach.call( arr, ( el, index ) => {
-    sum += el;
-  } );
-  return sum;
-}
